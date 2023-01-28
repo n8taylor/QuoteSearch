7b8cdbcfa4793f5b6a7b9628525b88861a960411
Nate Taylor <nate.taylor@aggiemail.usu.edu>
2023-01-27 23:22:25 -0700

    mkay

--- src/App.tsx
+++ src/App.tsx
@@ -68,7 +68,7 @@ function App() {
           placeholder="Albert Einstein"/>
         </form>
         <div>
-          <img src="../assets/magnifying-glass-svgrepo-com.svg" className="logo" alt="Magnifying glass" />
+          <img src="../public/magnifying-glass-svgrepo-com.svg" className="logo" alt="Magnifying glass" />
         </div>  
         {!searchPerformed && (
           <div id='random-quote'>
