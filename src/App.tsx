937a2c312e72bf9d7668274bfbe603700465aefd
Nate Taylor <nate.taylor@aggiemail.usu.edu>
2023-01-27 23:37:56 -0700

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import reactLogo from './assets/react.svg'
 import './App.css'
 import { QuoteBlob } from './components/QuoteBlob';
 
@@ -67,9 +66,12 @@ function App() {
           onChange={e => setSearch(e.target.value)} 
           placeholder="Albert Einstein"/>
         </form>
-        <div>
+        {/* <div>
           <img src="/magnifying-glass-svgrepo-com.svg" className="logo" alt="Magnifying glass" />
-        </div>  
+        </div>   */}
+      </div>
+      <br/>
+      <div>
         {!searchPerformed && (
           <div id='random-quote'>
             <h3>{quote != null ? quote.content : "Random quotes lead to random wisdom..."}</h3>
@@ -89,38 +91,8 @@ function App() {
           </div>
         )}
       </div>
-      <div>
-      </div>
     </div>
   );
-
-
-  // const [count, setCount] = useState(0)
-
-  // return (
-  //   <div className="App">
-  //     <div>
-  //       <a href="https://vitejs.dev" target="_blank">
-  //         <img src="/vite.svg" className="logo" alt="Vite logo" />
-  //       </a>
-  //       <a href="https://reactjs.org" target="_blank">
-  //         <img src={reactLogo} className="logo react" alt="React logo" />
-  //       </a>
-  //     </div>
-  //     <h1>Vite + React</h1>
-  //     <div className="card">
-  //       <button onClick={() => setCount((count) => count + 1)}>
-  //         count is {count}
-  //       </button>
-  //       <p>
-  //         Edit <code>src/App.tsx</code> and save to test HMR
-  //       </p>
-  //     </div>
-  //     <p className="read-the-docs">
-  //       Click on the Vite and React logos to learn more
-  //     </p>
-  //   </div>
-  // )
 }
 
 export default App
